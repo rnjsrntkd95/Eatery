83c9e51a77ba255d28865e5de98a5dcb97cde779
HyunJin Kim <hyunjinkim@HyunJinui-MacBook-Pro.local>
2019-08-06 15:27:45 +0900

    added GoogleMaps on HomeScreen

--- ios/EateryMap/AppDelegate.m
+++ ios/EateryMap/AppDelegate.m
@@ -11,10 +11,13 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyC4yRIZT518C8g9ODhb6Iu1RmGJF772Il8"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"EateryMap"
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,29 +2,41 @@ import React, { Component } from 'react'
 import {
     Text,
     View,
-    ScrollView,
+    FlatList,
     StyleSheet,
     Image
 } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 export default class HomeScreen extends Component {
     render(){
         const { navigate } = this.props.navigation;
         return(
-            <ScrollView style={styles.scrollView}>
-                <View>
-                    <Text>
-                        Hello World
-                    </Text>
-                </View>
-            </ScrollView>
+            <View style = {styles.container}>
+                <MapView
+                    provider = { PROVIDER_GOOGLE }
+                    style = { styles.map }
+                    region = {{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121  
+                    }}    
+                />
+            </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    scrollView: {
-        flex: 1,
-        backgroundColor: '#FFFFFF'
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject
     }
 })
