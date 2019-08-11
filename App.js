5ef0009dbd9ab845201c5df8b737a0fab70c7b3f
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2019-08-12 00:49:52 +0900

    make Button(TouchableOpacity) for registration

--- App.js
+++ App.js
@@ -5,30 +5,38 @@ import {
   Text,
   StyleSheet,
   TouchableHighlight,
+  TouchableOpacity,
+  
 } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      // <View style={{flexDirection: 'row'}}>
-      //   <View style={{backgroundColor: 'powderblue', flex:1}}/>
-      //   <Text style={styles.main}>가게명</Text>
-      // </View>
     <View style={{flex: 1}}>
 
       <View style={styles.name}>
         <Text style={{
           fontSize: 40,
           fontWeight: 'bold',
-        }}>Review</Text>
+        }}>  Review</Text>
         
         <Text style={{
           fontSize: 20,
           fontWeight: 'bold',
+          color: 'white'
         }}>EateryMap</Text>
 
       </View>
-      <View style={styles.ground}></View>
+      <View style={styles.ground}>
+        <TouchableOpacity 
+          onPress={this.onPressed}
+          style={styles.registerButton}
+        >
+          <Text style={{fontWeight: 'bold'}}>등록</Text>
+          
+        </TouchableOpacity>
+      </View>
+
 
     </View> 
     )
@@ -41,11 +49,27 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    padding: 5,
+    borderWidth: 2
   },
   ground: {
     flex: 9,
     backgroundColor: '#2f4f4f',
+    padding: 3,
+    
+    justifyContent: 'flex-end'
+  },
+  registerButton: {
+    alignItems: 'center',
+    backgroundColor: 'skyblue',
+    borderColor: 'blue',
+    borderWidth: 2,
+    borderRadius: 10,
+    paddingVertical: 3,
+    width: 60,
+    
+
   }
 
 });
